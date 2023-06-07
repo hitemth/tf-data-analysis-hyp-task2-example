ecef60e9a9df1f189ed052a6e12df0181e9ff7a1
hitemth <ivashkina.em@phystech.edu>
2023-06-08 02:02:44 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,10 +6,11 @@ import numpy as np
 chat_id = 156287560 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    from scipy.stats import ks_2samp
+    #from scipy.stats import ks_2samp
+    from scipy.stats import anderson_ksamp
 
-    stat, p_value = ks_2samp(x, y)
-    
-    # Отклоняем гипотезу однородности выборок, если p-value меньше уровня значимости
-    return p_value < 0.07
+    if anderson_ksamp([x, y]).significance_level < 0.07:
+        return True
+    else:
+        return False
 
