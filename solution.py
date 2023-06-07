5ef0af65902ebfeb1229763e1a38bf5de84eb51b
hitemth <ivashkina.em@phystech.edu>
2023-06-08 00:52:39 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,14 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+
+chat_id = 156287560 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    from scipy.stats import ks_2samp
+
+    stat, p_value = ks_2samp(x, y)
+    
+    # Отклоняем гипотезу однородности выборок, если p-value меньше уровня значимости
+    return p_value < 0.07
+
